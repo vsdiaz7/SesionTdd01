61db0911471d7b3860e8267a95f8b60a9003367c
Valentina Díaz <Valentina Daz>
2016-04-27 11:42:12 -0300

    testDosMasTresEsCinco

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
 public class CalculadoraTest {
-
+	@Test
+	public void dosMasTresEsCinco(){
+		Calculadora calculadora = new Calculadora();
+		int result = calculadora.suma(2,3);
+		assertThat(5, is(5));
+	}
 }
