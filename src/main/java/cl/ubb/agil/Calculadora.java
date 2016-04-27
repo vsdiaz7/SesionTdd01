e5f963edbb1edc16f1ef936467c7c4436e400620
Valentina Díaz <Valentina Daz>
2016-04-27 12:08:43 -0300

    PASO-10menos5es-15

--- src/main/java/cl/ubb/agil/Calculadora.java
+++ src/main/java/cl/ubb/agil/Calculadora.java
@@ -2,9 +2,9 @@ package cl.ubb.agil;
 
 public class Calculadora {
 	public int suma(int num1, int num2){
-		return num1+num2;
+		return (num1+num2);
 	}
 	public int resta(int num1, int num2){
-		return 127;
+		return (num1-num2);
 	}
 }
